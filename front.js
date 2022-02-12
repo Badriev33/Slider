63c6f24314b6b4b275b117ac9575a83acbd0d887
Avas <avsbadriev@mail.ru>
2022-02-12 18:23:42 +0300

    finish

--- front.js
+++ front.js
@@ -1,10 +1,9 @@
-fetch('bec.php')
+fetch('bec.php')// запрос на бек для получения данных
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    const a = (data)
-    console.log(a)
+    const a = (data) //получаем данные с сервера и нарезаем ссылки слайсами
     const img = a.slice(0, 56)
     const imgtwo = a.slice(58, 126)
     const imgthree = a.slice(128, 198)
@@ -14,3 +13,44 @@ fetch('bec.php')
     document.getElementById("userthree-avatar").src = imgthree;
 
   });
+
+const prev = document.getElementById('btn-prev')
+      next = document.getElementById('btn-next')
+      slides = document.querySelectorAll('.slide')
+      dots = document.querySelectorAll('.dot')
+
+index = 0
+
+const activeSlide = n => { //функция для активации слайдов
+  console.log(n)
+ for(slide of slides) {
+   slide.classList.remove('active')
+ }
+slides[n].classList.add('active')
+}
+
+const activeDot = n => {//функция для активации точек
+  for(dot of dots) {
+    dot.classList.remove('active')
+  }
+ dots[n].classList.add('active')
+ }
+
+
+
+const nextSlide = () => {//функция для кнопки next которая включает в себя функции активации для слайдов и точек
+  if(index ==slides.length - 1) {
+    index = 0
+    activeSlide(index)
+    activeDot(index)
+  } else {
+    index++
+    activeSlide(index)
+    activeDot(index)
+  }
+}
+
+
+  
+next.addEventListener('click', nextSlide)
+
